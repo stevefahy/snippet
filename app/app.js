8050f5b8cc82899bf68cc6c972f40f1ff78cf832
stevef <stevef0000@gmail.com>
2017-04-21 16:17:14 +0100

    Test sync with DO pm2

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Snippet!');
+  res.send('Snippet set up!');
 });
 
 app.listen(3010, function () {
