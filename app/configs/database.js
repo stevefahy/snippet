3fff94afa423ae12349fce7e3a54b8c1a83b929d
stevefahy <stevefahydev@gmail.com>
2017-07-31 13:46:29 +0100

    db

--- app/configs/database.js
+++ app/configs/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //remoteUrl: 'mongodb://127.0.0.1:27017/card',
-    remoteUrl: 'mongodb://127.0.0.1:50024/card/',
+    remoteUrl: 'mongodb://127.0.0.1:38028/card',
     localUrl: 'mongodb://localhost/card'
 };
