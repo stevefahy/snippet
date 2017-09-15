81526b8fe6b016be38739fa2b3cbbeb90db7eabd
stevefahy <stevefahydev@gmail.com>
2017-09-15 17:35:55 +0100

    base href

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.use(express.static('app'));
 
 app.use('/fileuploads', express.static(path.join(__dirname, '../upload_dir')));
 
+app.use('/snip', express.static(path.join(__dirname, '/app')));
+
 app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
