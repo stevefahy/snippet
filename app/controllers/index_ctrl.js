750e55c152eeb1366a33aeef2dd34dfe12e37b3b
stevefahy <stevefahydev@gmail.com>
2017-12-15 18:21:06 +0000

    notify

--- app/controllers/index_ctrl.js
+++ app/controllers/index_ctrl.js
@@ -1,5 +1,7 @@
 cardApp.controller("indexCtrl", ['$scope', 'Cards', 'replaceTags', '$rootScope', '$http', 'Format', 'Edit', '$window', '$routeParams', '$location', '$timeout', 'socket', function($scope, Cards, replaceTags, $rootScope, $http, Format, Edit, $window, $routeParams, $location, $timeout, socket) {
 
+    var ua = navigator.userAgent;
+
     // Get the current users details
     $http.get("/api/user_data").then(function(result) {
         if (result.data.user) {
@@ -8,6 +10,13 @@ cardApp.controller("indexCtrl", ['$scope', 'Cards', 'replaceTags', '$rootScope',
             // and request that a unique namespace be created for this user with their user id
             socket.setId(result.data.user._id);
             socket.connect(socket.getId());
+
+            //
+            console.log('running?');
+            if (ua === 'AndroidApp') {
+                console.log('sendDetails');
+                Android.sendDetails(result.data.user._id);
+            }
         }
     });
 
