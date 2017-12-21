ba39648c74b314a229a14bf2a5a42e4f839517b9
stevefahy <stevefahydev@gmail.com>
2017-12-21 21:55:39 +0000

    notify

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -446,7 +446,7 @@ module.exports = function(app, passport) {
                                 "operation": "add",
                                 "notification_key_name": req.user._id,
                                 "notification_key": user.notification_key,
-                                "registration_ids": token_array
+                                "registration_ids": [token_array]
                             };
 
                             var new_headers = {
@@ -510,7 +510,7 @@ module.exports = function(app, passport) {
                             "operation": "add",
                             "notification_key_name": req.user._id,
                             "notification_key": new_user.notification_key,
-                            "registration_ids": token_array
+                            "registration_ids": [token_array]
                         };
 
                         var new_headers = {
