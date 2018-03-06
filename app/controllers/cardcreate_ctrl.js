b7060f6c1a43f5e82c5bf9c78c37ee36413bd19e
stevefahy <stevefahydev@gmail.com>
2018-03-06 12:00:40 +0000

    preclean

--- app/controllers/cardcreate_ctrl.js
+++ app/controllers/cardcreate_ctrl.js
@@ -1,4 +1,4 @@
-cardApp.controller("cardcreateCtrl", ['$scope', '$rootScope', '$location', '$http', '$window', '$timeout', 'Cards', 'replaceTags', 'Format', 'Edit', 'Conversations', 'socket', 'Users', 'Database', function($scope, $rootScope, $location, $http, $window, $timeout, Cards, replaceTags, Format, Edit, Conversations, socket, Users, Database) {
+cardApp.controller("cardcreateCtrl", ['$scope', '$rootScope', '$location', '$http', '$window', '$timeout', 'Cards', 'replaceTags', 'Format', 'FormatHTML', 'Edit', 'Conversations', 'socket', 'Users', 'Database', function($scope, $rootScope, $location, $http, $window, $timeout, Cards, replaceTags, Format, FormatHTML, Edit, Conversations, socket, Users, Database) {
 
     var ua = navigator.userAgent;
 
@@ -11,53 +11,95 @@ cardApp.controller("cardcreateCtrl", ['$scope', '$rootScope', '$location', '$htt
     $scope.keyListen = Format.keyListen;
     $scope.showAndroidToast = Format.showAndroidToast;
     $scope.uploadFile = Format.uploadFile;
-    $scope.myFunction = Edit.myFunction;
+    //$scope.myFunction = Edit.myFunction;
+    $scope.restoreSelection = Format.restoreSelection;
+
+    $scope.saveSelection = Format.saveSelection;
+    $scope.checkCursor = Format.checkCursor;
 
     $scope.input = false;
-
-
-    this.$onInit = function() {
-
-    };
+    var isFocused = false;
 
     // Add the input prompt attribute
     $timeout(function() {
-        $('#cecard_create').attr('data-placeholder', INPUT_PROMPT);
-        console.log(ua);
-        if (ua.toLowerCase().indexOf('firefox') > -1 || ua.toLowerCase().indexOf('edge') > -1) {
 
 
-            $('#cecard_create').on('focus', function() {
-                console.log('FOCUS');
-                var $this = $(this);
-                $this.html($this.html() + '<br>'); // firefox hack
-
-
-  
-            });
-
-
-            $('#cecard_create').on('blur', function() {
-                var $this = $(this);
-                $this.text($this.text().replace('<.*?>', ''));
+        $('#cecard_create').on('focus', function() {
+            $scope.focused = true;
+            //$timeout(function() {
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+            //});
+            //$scope.$apply();
+            if (ua.toLowerCase().indexOf('firefox') > -1 || ua.toLowerCase().indexOf('edge') > -1) {
+                //var $this = $(this);
+                //$this.html($this.html() + '<br>'); // firefox hack
+            }
 
 
 
-            });
+        });
+
+
+        $('#cecard_create').on('blur', function() {
+            //console.log('BLUR');
+            $scope.focused = false;
+            /*
+            var last_focus = document.activeElement;
+            console.log(last_focus);
+            var caretPosEl = document.getElementById("caretposition");
+            caretPosEl.innerHTML = "Last focused: " + last_focus;
+            */
+
+            if (ua.toLowerCase().indexOf('firefox') > -1 || ua.toLowerCase().indexOf('edge') > -1) {
+                //var $this = $(this);
+                //$this.text($this.text().replace('<.*?>', ''));
+            }
+        });
+
+    });
+
+    checkInput = function() {
+
+    };
+
+    imageLoaded = function() {
+        console.log('imageLoaded');
+        //checkInput();
+        $scope.input = true;
+        $('#placeholderDiv').hide();
+    };
+
+    $(document).on('input keyup', '#cecard_create', function() {
+        var trim = $.trim($('#cecard_create').text());
+        console.log('trim : ' + trim);
+        // check for whitespace at first position
+        if (trim.length == 1 && trim.charCodeAt(0) == '8203') {
+            $('#cecard_create').html('');
+        }
+        //if ($.trim($('#cecard_create').text()).length > 0) {
+        if ($('#cecard_create').text().length > 0) {
+            $('#placeholderDiv').hide();
+            $scope.input = true;
+        } else {
+            $('#placeholderDiv').show();
+            $scope.input = false;
         }
     });
+
 
     // Add the input prompt listener
     (function($) {
         $(document).on('change keydown keypress input', 'div[data-placeholder]', function() {
             if (this.textContent) {
                 $scope.input = true;
-                this.dataset.divPlaceholderContent = 'true';
+                //this.dataset.divPlaceholderContent = 'true';
 
             } else {
                 $scope.input = false;
-//window.getSelection().removeAllRanges();
-                delete(this.dataset.divPlaceholderContent);
+                //window.getSelection().removeAllRanges();
+                //delete(this.dataset.divPlaceholderContent);
             }
         });
 
@@ -68,7 +110,7 @@ cardApp.controller("cardcreateCtrl", ['$scope', '$rootScope', '$location', '$htt
 
     $scope.card_create = {
         _id: 'card_create',
-        content: '',
+        content: "",
         //user: $scope.currentUser,
         user_name: ''
     };
@@ -82,6 +124,31 @@ cardApp.controller("cardcreateCtrl", ['$scope', '$rootScope', '$location', '$htt
         //
         $scope.input = false;
     });
+
+    $scope.media = function() {
+        console.log('isFocused: ' + isFocused);
+
+        if (isFocused) {
+            console.log('media');
+            //$scope.restoreCaret('cecard_create');
+            //$scope.restoreSelection('cecard_create');
+            //console.log('savedSelection: ' + savedSelection);
+            // if (savedSelection) {
+            $scope.restoreSelection(document.getElementById("cecard_create"));
+            // }
+
+            $scope.uploadFile();
+        }
+    };
+
+    $scope.checkFocus = function() {
+        console.log('checkFocus');
+        if ($scope.focused) {
+            isFocused = true;
+        } else {
+            isFocused = false;
+        }
+    };
 
     // Create Card
     $scope.createCard = function(id, card_create) {
@@ -102,6 +169,7 @@ cardApp.controller("cardcreateCtrl", ['$scope', '$rootScope', '$location', '$htt
     if (ua !== 'AndroidApp') {
         $window.onfocus = function() {
             //this.setFocus();
+
         };
         $window.focus();
         setFocus();
