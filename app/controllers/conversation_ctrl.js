8d61ac5c667f77e38b01e9c6fb196edec6976a6c
stevefahy <stevefahydev@gmail.com>
2018-03-16 11:50:59 +0000

    footer

--- app/controllers/conversation_ctrl.js
+++ app/controllers/conversation_ctrl.js
@@ -176,15 +176,15 @@ cardApp.controller("conversationCtrl", ['$scope', '$rootScope', '$location', '$h
         //if (focused.id == 'cecard_create') {
             // TODO enter is a pasteHTML. need to add scroll into view for android
             // Paste div so that scrollintoview works on Android
-            $scope.pasteHtmlAtCaret("<span class='scroll_latest_footer' id='scroll_latest_footer'></span>");
+            $scope.pasteHtmlAtCaret("<div class='scroll_latest_footer' id='scroll_latest_footer'></div>");
             console.log(document.getElementById('scroll_latest_footer'));
             $timeout(function() {
             document.getElementById('scroll_latest_footer').scrollIntoView({ behavior: "instant", block: "nearest", inline: "nearest" });
-            }, 500);
+            }, 200);
             // remove scroll_latest after scrolling
             $timeout(function() {
                 $(".scroll_latest_footer").remove();
-            }, 700);
+            }, 300);
         //}
     };
 
