fb0f3c0815b27ac50504d4e3e2985a8623edfe0d
stevefahy <stevefahydev@gmail.com>
2018-07-02 21:06:27 +0100

    network

--- app.js
+++ app.js
@@ -153,9 +153,31 @@ mongoose.connect(dburl); // Connect to local MongoDB instance. A remoteUrl is al
 //mongoose.connect(dbuserurl); // User login
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+//db.on('error', console.error.bind(console, 'connection error:'));
+//db.once('open', function() {
+    //console.log('mongoose open');
+//});
 
+db.on('connecting', function() {
+    console.log('connecting to MongoDB...');
+});
+
+db.on('error', function(error) {
+    console.error('Error in MongoDb connection: ' + error);
+    mongoose.disconnect();
+});
+db.on('connected', function() {
+    console.log('MongoDB connected!');
+});
+db.once('open', function() {
+    console.log('MongoDB connection opened!');
+});
+db.on('reconnected', function() {
+    console.log('MongoDB reconnected!');
+});
+db.on('disconnected', function() {
+    console.log('MongoDB disconnected!');
+    mongoose.connect(dburl, { server: { auto_reconnect: true } });
 });
 
 
