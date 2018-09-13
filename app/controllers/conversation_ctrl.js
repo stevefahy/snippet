7cf3cc52182e2709aea163e08e5995e2d277ad4d
stevefahy <stevefahydev@gmail.com>
2018-09-13 18:49:03 +0100

    fltr

--- app/controllers/conversation_ctrl.js
+++ app/controllers/conversation_ctrl.js
@@ -553,7 +553,7 @@ $rootScope.last_win_width;
     }
     };
 
-$scope.inviewoptions = {offset:[1000, 0, 1000, 0]};
+$scope.inviewoptions = {offset:[5000, 0, 5000, 0]};
 
 
 $scope.lineInView = function (data, id) {
