e40e347cde1b11035edb0b0171c5b373f34a0988
stevefahy <stevefahydev@gmail.com>
2019-02-25 16:10:19 +0000

    lazy

--- app/controllers/conversation_ctrl.js
+++ app/controllers/conversation_ctrl.js
@@ -220,7 +220,8 @@ cardApp.controller("conversationCtrl", ['$scope', '$rootScope', '$location', '$h
         var currentScroll = $(this).scrollTop();
         var maxScroll = this.scrollHeight - this.clientHeight;
         var scrolled2 = (currentScroll / maxScroll) * 100;
-        console.log('scrolled2: ' + scrolled2 + ' == TOP_END ' + TOP_END + ' no_more_records: ' + no_more_records + ' $scope.removed_cards_top.length: ' + $scope.removed_cards_top.length + ' $scope.removed_cards_bottom.length: ' + $scope.removed_cards_bottom.length + ', $scope.card_temp.length: ' + $scope.card_temp.length + ' update_adjust: ' + update_adjust);
+
+        console.log('scrolled2: ' + scrolled2 + ' == TOP_END ' + TOP_END + ' no_more_records: ' + no_more_records + ' $scope.removed_cards_top.length: ' + $scope.removed_cards_top.length + ' $scope.removed_cards_bottom.length: ' + $scope.removed_cards_bottom.length + ' $scope.cards_temp.length: ' + $scope.cards_temp.length + ' update_adjust: ' + update_adjust);
         if (scrolled2 <= TOP_END && (!no_more_records || $scope.removed_cards_top.length > 0  || $scope.card_temp.length > 0) && !update_adjust) {
             console.log('TOP!');
             extremity = true;
