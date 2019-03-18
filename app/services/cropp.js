fb4221ca7f7ee73f11cb3819f2584c015fe5b9ee
stevefahy <stevefahydev@gmail.com>
2019-03-18 20:35:58 +0000

    drag

--- app/services/cropp.js
+++ app/services/cropp.js
@@ -145,6 +145,112 @@ cardApp.service('Cropp', ['$window', '$rootScope', '$timeout', '$q', '$http', 'U
         }
     }
 
+    /*Make resizable div by Hung Nguyen*/
+    function makeResizableDiv(div) {
+        const element = document.querySelector(div);
+        const resizers = document.querySelectorAll(div + ' .resizer')
+        const minimum_size = 20;
+        let original_width = 0;
+        let original_height = 0;
+        let original_x = 0;
+        let original_y = 0;
+        let original_mouse_x = 0;
+        let original_mouse_y = 0;
+        for (let i = 0; i < resizers.length; i++) {
+            const currentResizer = resizers[i];
+
+            //elmnt.addEventListener("touchstart", dragMouseDown, false);
+            currentResizer.addEventListener("mousedown", sizeMouseDown, false);
+            currentResizer.addEventListener("touchstart", sizeMouseDown, false);
+
+            /*
+            currentResizer.addEventListener('mousedown', function(e) {
+                e.preventDefault()
+                original_width = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
+                original_height = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
+                original_x = element.getBoundingClientRect().left;
+                original_y = element.getBoundingClientRect().top;
+                original_mouse_x = e.pageX;
+                original_mouse_y = e.pageY;
+                window.addEventListener('mousemove', resize)
+                window.addEventListener('mouseup', stopResize)
+            });
+            */
+
+            function sizeMouseDown(e) {
+                original_width = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
+                original_height = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
+                original_x = element.getBoundingClientRect().left;
+                original_y = element.getBoundingClientRect().top;
+                original_mouse_x = e.pageX;
+                original_mouse_y = e.pageY;
+                if (!mobile) {
+                    window.addEventListener('mousemove', resize);
+                    window.addEventListener('mouseup', stopResize);
+                } else {
+                    currentResizer.addEventListener("touchmove", resize, false);
+                    currentResizer.addEventListener("touchend", stopResize, false);
+                }
+
+            }
+
+            function resize(e) {
+                if (currentResizer.classList.contains('bottom-right')) {
+                    const width = original_width + (e.pageX - original_mouse_x);
+                    const height = original_height + (e.pageY - original_mouse_y)
+                    if (width > minimum_size) {
+                        element.style.width = width + 'px'
+                    }
+                    if (height > minimum_size) {
+                        element.style.height = height + 'px'
+                    }
+                } else if (currentResizer.classList.contains('bottom-left')) {
+                    const height = original_height + (e.pageY - original_mouse_y)
+                    const width = original_width - (e.pageX - original_mouse_x)
+                    if (height > minimum_size) {
+                        element.style.height = height + 'px'
+                    }
+                    if (width > minimum_size) {
+                        element.style.width = width + 'px'
+                        element.style.left = original_x + (e.pageX - original_mouse_x) + 'px'
+                    }
+                } else if (currentResizer.classList.contains('top-right')) {
+                    const width = original_width + (e.pageX - original_mouse_x)
+                    const height = original_height - (e.pageY - original_mouse_y)
+                    if (width > minimum_size) {
+                        element.style.width = width + 'px'
+                    }
+                    if (height > minimum_size) {
+                        element.style.height = height + 'px'
+                        element.style.top = original_y + (e.pageY - original_mouse_y) + 'px'
+                    }
+                } else {
+                    const width = original_width - (e.pageX - original_mouse_x)
+                    const height = original_height - (e.pageY - original_mouse_y)
+                    if (width > minimum_size) {
+                        element.style.width = width + 'px'
+                        element.style.left = original_x + (e.pageX - original_mouse_x) + 'px'
+                    }
+                    if (height > minimum_size) {
+                        element.style.height = height + 'px'
+                        element.style.top = original_y + (e.pageY - original_mouse_y) + 'px'
+                    }
+                }
+            }
+
+            function stopResize() {
+                if(!mobile){
+                    window.removeEventListener('mousemove', resize);
+                } else {
+                     currentResizer.removeEventListener("touchmove", resize, false);
+                }
+                
+            }
+        }
+    }
+
+
+
     this.openCrop = function(e, id) {
         var parent_container;
         // Get the context of the image (content_cnv or card_create_container)
@@ -174,21 +280,23 @@ cardApp.service('Cropp', ['$window', '$rootScope', '$timeout', '$q', '$http', 'U
             //$('.' + parent_container + ' #cropper_' + id).addClass(filter);
             filter = JSON.parse(filter);
             var img = $('.' + parent_container + ' #image_' + id).clone().appendTo('.' + parent_container + ' #cropper_' + id + ' .crop_area');
-        $(img).addClass(filter.filter);
-         $('.' + parent_container + ' #image_' + id).css('display', 'inline');
-         $('.' + parent_container + ' #image_' + id).addClass(filter.filter);
+            $(img).addClass(filter.filter);
+            $('.' + parent_container + ' #image_' + id).css('display', 'inline');
+            $('.' + parent_container + ' #image_' + id).addClass(filter.filter);
 
         } else {
             var img = $('.' + parent_container + ' #image_' + id).clone().appendTo('.' + parent_container + ' #cropper_' + id + ' .crop_area');
-        
+
         }
 
-                $(img).attr('id', 'crop_src');
+        $(img).attr('id', 'crop_src');
         $('.' + parent_container + ' #cropper_' + id + ' .crop_adjust').attr('id', 'drag');
 
 
         //Make the DIV element draggagle:
         dragElement(document.getElementById("drag"));
+
+        makeResizableDiv('.crop_adjust');
     };
 
     /*
