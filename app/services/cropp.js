6e5177246789fbc369873bf202aa93bb183e0dc6
stevefahy <stevefahydev@gmail.com>
2019-04-01 12:47:31 +0100

    crop

--- app/services/cropp.js
+++ app/services/cropp.js
@@ -651,8 +651,8 @@ cardApp.service('Cropp', ['$window', '$rootScope', '$timeout', '$q', '$http', 'U
             Format.setImageEditing(true);
             //$('.' + parent_container + ' #cropper_' + id).closest('div.ce').focus();
             //$('.' + parent_container + ' #cropper_' + id).closest('div.ce').blur();
-            console.log( $('.' + parent_container + ' #cropper_' + id).closest('div#user'));
-            $('.' + parent_container + ' #cropper_' + id).closest('div#user').focus();
+            console.log( $('.' + parent_container + ' #cropper_' + id).closest('div.card_temp'));
+            $('.' + parent_container + ' #cropper_' + id).closest('div.card_temp').focus();
             //$('#hidden_input_container').focus();
             
             $timeout(function() {
