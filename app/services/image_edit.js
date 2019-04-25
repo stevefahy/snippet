63cde5f31e020aeac13ec40575580451ab21e7d8
stevefahy <stevefahydev@gmail.com>
2019-04-25 11:10:13 +0100

    rotate

--- app/services/image_edit.js
+++ app/services/image_edit.js
@@ -16,16 +16,87 @@ cardApp.service('ImageEdit', ['$window', '$rootScope', '$timeout', '$q', '$http'
         mobile = true;
     }
 
-    $rootScope.sliderRotateChange =function(amount){
-         self.sliderRotateChange(amount);
+    $rootScope.slider_settings = {
+
+        sharpen: {
+            amount: 0,
+            reset: 0,
+            options: {
+                floor: 0,
+                ceil: 20,
+                step: 0.1,
+                precision: 1,
+                id: 'slider-id',
+                onStart: function(sharpen) {
+                    //console.log('on start ' + $rootScope.slider_settings.sharpen.amount);
+                },
+                onChange: function(id) {
+                    //console.log('on change ' + $rootScope.slider_settings.sharpen.amount);
+                },
+                onEnd: function(id) {
+                    //console.log('on end ' + $rootScope.slider_settings.sharpen.amount);
+                    ImageAdjustment.setImageAdjustment(ImageAdjustment.getImageParent(), ImageAdjustment.getImageId(), 'sharpen', $rootScope.slider_settings.sharpen.amount);
+                    ImageAdjustment.setSharpenUpdate(ImageAdjustment.getSource(), ImageAdjustment.getTarget(), ImageAdjustment.getImageAdjustments(ImageAdjustment.getImageParent(), ImageAdjustment.getImageId()));
+                }
+            }
+        },
+
+        rotate: {
+            amount: 0,
+            reset: 0,
+            options: {
+                floor: -45,
+                ceil: 45,
+                step: 0.1,
+                precision: 1,
+                id: 'slider-idt',
+                onStart: function(sharpen) {
+                    //console.log('on start ' + $rootScope.slider_settings.rotate.amount);
+                },
+                onChange: function(id) {
+                    //console.log('on change ' + $rootScope.slider_settings.rotate.amount);
+                    self.sliderRotateChange($rootScope.slider_settings.rotate.amount);
+                },
+                onEnd: function(id) {
+                    //console.log('on end ' + $rootScope.slider_settings.rotate.amount);
+                }
+            }
+        },
+
+        skew_h: {
+            amount: 0,
+            reset: 0,
+            options: {
+                floor: -1,
+                ceil: 1,
+                step: 0.1,
+                precision: 1,
+                id: 'slider-idt',
+                onStart: function(sharpen) {
+                    //console.log('on start ' + $rootScope.slider_settings.rotate.amount);
+                },
+                onChange: function(id) {
+                    //console.log('on change ' + $rootScope.slider_settings.rotate.amount);
+                    self.sliderSkewHChange($rootScope.slider_settings.skew_h.amount);
+                },
+                onEnd: function(id) {
+                    //console.log('on end ' + $rootScope.slider_settings.rotate.amount);
+                }
+            }
+        }
+
+    };
+
+    $rootScope.sliderRotateChange = function(amount) {
+        self.sliderRotateChange(amount);
     };
 
 
-    $rootScope.sliderSkewHChange =function(amount){
-         //self.sliderRotateChange(amount);
-         console.log(amount);
+    $rootScope.sliderSkewHChange = function(amount) {
+        //self.sliderRotateChange(amount);
+        console.log(amount);
     };
-    
+
 
     // Helper functions
 
