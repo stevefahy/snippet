95e489c40b80106202bbfba9655b6598a9cc5fb1
stevefahy <stevefahydev@gmail.com>
2019-06-14 20:34:57 +0100

    edit

--- app/services/image_edit.js
+++ app/services/image_edit.js
@@ -863,7 +863,7 @@ cardApp.service('ImageEdit', ['$window', '$rootScope', '$timeout', '$q', '$http'
         var mc = new Hammer(cropper);
         // let the pan gesture support all directions.
         // this will block the vertical scrolling on a touch-device while on the element
-        mc.get('pan').set({ direction: Hammer.DIRECTION_ALL, threshold: 10 });
+        mc.get('pan').set({ direction: Hammer.DIRECTION_ALL, threshold: 1 });
         // listen to events...
 
         mc.on("panleft panright panup pandown tap press panstart panend", function(ev) {
