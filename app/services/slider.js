28ff4821b1c1b914fd2f6763544d8fe2ff5202ca
stevefahy <stevefahydev@gmail.com>
2019-06-16 21:11:27 +0100

    edit

--- app/services/slider.js
+++ app/services/slider.js
@@ -5,9 +5,9 @@
 cardApp.service('Slider', ['$window', '$rootScope', 'ImageAdjustment', function($window, $rootScope, ImageAdjustment) {
 
     this.slider_sharpen = '<rzslider rz-slider-model="slider_settings.sharpen.amount" rz-slider-options="slider_settings.sharpen.options" </rzslider>';
-    this.slider_rotate = '<div class="slider_r"><div class="slider_r_icon"><i class="material-icons light rotate">autorenew</i></div><rzslider rz-slider-model="slider_settings.rotate.amount" rz-slider-options="slider_settings.rotate.options" id="slider_r"></rzslider></div>';
-    this.slider_perspective_v = '<rzslider rz-slider-model="slider_settings.perspective_v.amount" rz-slider-options="slider_settings.perspective_v.options" id="slider_p_v"></rzslider>';
-    this.slider_perspective_h = '<rzslider rz-slider-model="slider_settings.perspective_h.amount" rz-slider-options="slider_settings.perspective_h.options" id="slider_p_h"></rzslider>';
+    this.slider_rotate = '<div class="slider_outer"><div class="slider_r_icon"><i class="material-icons light rotate">autorenew</i></div><rzslider rz-slider-model="slider_settings.rotate.amount" rz-slider-options="slider_settings.rotate.options" id="slider_r"></rzslider></div>';
+    this.slider_perspective_v = '<div class="slider_outer"><div class="slider_p_v_icon"><img class="graphic-icons" src="/assets/images/perspective_w.png"></div><rzslider rz-slider-model="slider_settings.perspective_v.amount" rz-slider-options="slider_settings.perspective_v.options" id="slider_p_v"></rzslider></div>';
+    this.slider_perspective_h = '<div class="slider_outer"><div class="slider_p_h_icon"><img class="graphic-icons" src="/assets/images/perspective_w.png"></div><rzslider rz-slider-model="slider_settings.perspective_h.amount" rz-slider-options="slider_settings.perspective_h.options" id="slider_p_h"></rzslider></div>';
     this.slider_test = '<rzslider rz-slider-model="slider_settings.test.amount" rz-slider-options="slider_settings.test.options"></rzslider>';
 
     $rootScope.slider_touch_settings = {
