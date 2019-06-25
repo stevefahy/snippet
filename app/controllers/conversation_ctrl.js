8ee6931d9c9b528e33a750dd7b21c4a2aedc07ac
stevefahy <stevefahydev@gmail.com>
2019-06-25 17:28:26 +0100

    £edit

--- app/controllers/conversation_ctrl.js
+++ app/controllers/conversation_ctrl.js
@@ -798,6 +798,10 @@ cardApp.controller("conversationCtrl", ['$scope', '$rootScope', '$location', '$h
         $('.slider_container').addClass('animate');
         var currentHeight = $('.slider_container_inner').outerHeight();
         $('.slider_container').css('height', currentHeight);
+
+        //var mb = parseInt($('.content_cnv').css('margin-bottom'));
+        //console.log(mb);
+         //$('.content_cnv').css('margin-bottom', mb - currentHeight + 'px');
         s.removeClass('hide');
     },0);
     };
