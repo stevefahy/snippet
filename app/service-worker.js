19509a5ec12df290ab4f20cba3ebdcee6b729b96
stevefahy <stevefahydev@gmail.com>
2019-10-22 22:24:09 +0100

    offline

--- app/service-worker.js
+++ app/service-worker.js
@@ -64,47 +64,7 @@ if (workbox) {
 
     });
 
-    //client to SW
-    self.addEventListener('message', function(event) {
-        if (event.data === 'replayRequests') {
-            syncPosts();
-        }
-    });
-
-    //const queue = new workbox.backgroundSync.Queue('api_posts');
-
-
-
-    async function syncPosts(queue) {
-        //console.log('...Synchronizing ' + queue.name);
-        let entry;
-        let clone;
-        let response;
-        while (entry = await queue.shiftRequest()) {
-            try {
-                clone = await entry.request.clone();
-                console.log('...Replaying: ' + entry.request.url);
-                send_message_to_all_clients({ message: 'post_updating' });
-                response = await fetch(entry.request);
-                console.log(response);
-                let requestData = await clone.json();
-                console.log(requestData);
-                let assetsData = await response.json();
-                console.log(assetsData);
-                var card_data = { temp: requestData, posted: assetsData };
-                console.log('...Replayed: ' + entry.request.url);
-                send_message_to_all_clients({ message: 'post_updated', data: card_data });
-            } catch (error) {
-                console.error('Replay failed for request', entry.request, error);
-                await queue.unshiftRequest(entry);
-                return;
-            }
-        }
-        send_message_to_all_clients({message: 'all_posts_updated'});
-        console.log('Replay complete!');
-    }
-
-    const queue = new workbox.backgroundSync.Queue('api_posts', {
+const queue = new workbox.backgroundSync.Queue('api_posts', {
         onSync: async ({ queue }) => {
             let entry;
             let clone;
@@ -133,6 +93,48 @@ if (workbox) {
             console.log('Replay complete!');
         }
     });
+
+    //client to SW
+    self.addEventListener('message', function(event) {
+        if (event.data === 'replayRequests') {
+            syncPosts();
+        }
+    });
+
+    //const queue = new workbox.backgroundSync.Queue('api_posts');
+
+
+
+    async function syncPosts() {
+        console.log('...Synchronizing ' + queue.name);
+        let entry;
+        let clone;
+        let response;
+        while (entry = await queue.shiftRequest()) {
+            try {
+                clone = await entry.request.clone();
+                console.log('...Replaying: ' + entry.request.url);
+                send_message_to_all_clients({ message: 'post_updating' });
+                response = await fetch(entry.request);
+                console.log(response);
+                let requestData = await clone.json();
+                console.log(requestData);
+                let assetsData = await response.json();
+                console.log(assetsData);
+                var card_data = { temp: requestData, posted: assetsData };
+                console.log('...Replayed: ' + entry.request.url);
+                send_message_to_all_clients({ message: 'post_updated', data: card_data });
+            } catch (error) {
+                console.error('Replay failed for request', entry.request, error);
+                await queue.unshiftRequest(entry);
+                return;
+            }
+        }
+        send_message_to_all_clients({message: 'all_posts_updated'});
+        console.log('Replay complete!');
+    }
+
+    
 
     const rest_fail = {
         // If the request fails then add this REST Post to the queue.
